213e32d3446c4db0ce601cfd5eadbf75a3485bd0
Samu Warinowski <samu.warinowski@gmail.com>
2021-12-31 20:19:49 +0200

    Added scalability

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,7 +12,7 @@ const showMenu = (toggleId, navId) => {
   }
 }
 
-showMenu('nav-toggle', 'nav-menu')
+showMenu('nav-toggle','nav-menu')
 
 // remove menu mobile
 
@@ -32,16 +32,77 @@ const sections = document.querySelectorAll('section[id]')
 function scrollActive() {
   const scrollY = window.pageYOffset
 
-  sections.forEach(current => {
+  sections.forEach(current =>{
     const sectionHeight = current.offsetHeight
     const sectionTop = current.offsetTop - 50;
     sectionId = current.getAttribute('id')
 
-    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
       document.queryselector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-    } else {
+    }else{
       document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
     }
   })
 }
 window.addEventListener('scroll', scrollActive)
+
+// Show Scroll Top
+
+function scrollTop(){
+    const scrollTop = document.getElementById('scroll-top');
+    if(this.scrollY >= 200) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollTop)
+
+// Dark/Light Theme
+
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'bx-sun'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// Obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-moon' : 'bx-sun'
+
+// Validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, ask what the issue was to know if dark is activated
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // Save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+// Reduce size and print on A4 sheet
+function scaleCv(){
+  document.body.classList.add('scale-cv')
+}
+
+//Remove the size when cv is downloaded
+
+function removeScale(){
+  document.body.classList.remove('scale-cv')
+}
+
+//Generate PDF
+
+//PDF Generated area
+let resumeButton = document.getElementById('resume-button')
+
+
+resumeButton.addEventListener('click', () =>{
+// 1. The class .scale-cv is added to body, where it reduces the size
+  scaleCv()
+})
